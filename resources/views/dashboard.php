a125341011b16f07f193a49a8124e0ff6a406e90
Luke Moore <12770383+lukewmoore@users.noreply.github.com>
2020-06-10 10:08:58 +1000

    Show navigation on dashboard

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -3,7 +3,15 @@
  * Dashboard view.
  *
  * @package MemberFrontend
+ *
+ * @var \App\Plugins\Pvtl\Classes\Member_Frontend $this
  */
 
+$this->partial( 'notices' );
+
 ?>
+<h2>Hi <?php echo esc_html( $user->user_firstname ); ?></h2>
+
+<?php $this->partial( 'nav' ); ?>
+
 <?php echo $content; // phpcs:ignore ?>
