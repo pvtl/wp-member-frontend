e1c89bf7c761409d871e365a52babe88a4c2cf6d
Luke Moore <12770383+lukewmoore@users.noreply.github.com>
2020-06-10 10:07:41 +1000

    Updated nav links

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -3,13 +3,18 @@
  * Member navigation view.
  *
  * @package MemberFrontend
+ *
+ * @var \App\Plugins\Pvtl\Classes\Member_Frontend $this
  */
 
 ?>
 <nav class="member-nav">
 	<ul>
 		<li>
-			<a href="<?php echo $this->get_page_url( 'dashboard' ); ?>">Dashboard</a>
+			<a href="<?php echo $this->url( 'dashboard' ); ?>">Dashboard</a>
+		</li>
+		<li>
+			<a href="<?php echo $this->url( 'account' ); ?>">Account</a>
 		</li>
 		<li>
 			<a href="<?php echo $this->get_logout_url(); ?>">Logout</a>
