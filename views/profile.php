36024f985d2528347784615fd1857046fab77f5f
Luke Moore <12770383+lukewmoore@users.noreply.github.com>
2020-12-02 09:25:55 +1000

    Tidy up profile view

--- views/profile.php
+++ views/profile.php
@@ -20,39 +20,33 @@
 
 	<?php $this->partial( 'notices' ); ?>
 
-	<fieldset>
-		<div class="form-group">
-			<label for="first_name">First Name</label>
-			<input id="first_name" name="first_name" type="text" class="form-control" value="<?php echo esc_html( $user->user_firstname ); ?>" autocomplete="given-name">
-		</div>
+	<div class="form-group">
+		<label for="first_name">First Name</label>
+		<input id="first_name" name="first_name" type="text" class="form-control" value="<?php echo esc_html( $user->user_firstname ); ?>" autocomplete="given-name">
+	</div>
 
-		<div class="form-group">
-			<label for="last_name">Last Name</label>
-			<input id="last_name" name="last_name" type="text" class="form-control" value="<?php echo esc_html( $user->user_lastname ); ?>" autocomplete="family-name">
-		</div>
+	<div class="form-group">
+		<label for="last_name">Last Name</label>
+		<input id="last_name" name="last_name" type="text" class="form-control" value="<?php echo esc_html( $user->user_lastname ); ?>" autocomplete="family-name">
+	</div>
 
-		<div class="form-group">
-			<label for="email">Email</label>
-			<input id="email" name="email" type="email" class="form-control" value="<?php echo esc_html( $user->user_email ); ?>" autocomplete="email">
-		</div>
-	</fieldset>
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input id="email" name="email" type="email" class="form-control" value="<?php echo esc_html( $user->user_email ); ?>" autocomplete="email">
+	</div>
 
 	<h3>Change your password</h3>
 	<p>If you would like to change your password, enter a new one here.</p>
 
-	<fieldset>
-		<div class="form-group">
-			<label for="password">New password</label>
-			<input id="password" name="password" type="password" class="form-control" autocomplete="new-password">
-		</div>
-
-		<div class="form-group">
-			<label for="confirm_password">Confirm password</label>
-			<input id="confirm_password" name="confirm_password" type="password" class="form-control" autocomplete="new-password">
-		</div>
-	</fieldset>
-
-	<div>
-		<button type="submit" class="btn btn-primary">Update Profile</button>
+	<div class="form-group">
+		<label for="password">New password</label>
+		<input id="password" name="password" type="password" class="form-control" autocomplete="new-password">
 	</div>
+
+	<div class="form-group">
+		<label for="confirm_password">Confirm password</label>
+		<input id="confirm_password" name="confirm_password" type="password" class="form-control" autocomplete="new-password">
+	</div>
+
+	<button type="submit" class="btn btn-primary">Update Profile <span style="display: none;" class="spinner"><i class="fal fa-spinner-third fa-spin"></i></span></button>
 </form>
